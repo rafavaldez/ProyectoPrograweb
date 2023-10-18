2ff5e26d7c22acc3b115a33a140e52d195ab275d
Rafael V <81579515+rafavaldez@users.noreply.github.com>
2023-10-18 12:24:04 -0500

    Plantilla default

---
+++ Proyecto_Grupal_PrograWeb2/Controllers/GameVozController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Grupal_PrograWeb2.Controllers
+{
+    public class GameVozController : Controller
+    {
+        // GET: GameVoz
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JuegoVoz()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyecto_Grupal_PrograWeb2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Grupal_PrograWeb2.MvcApplication" Language="C#" %>
